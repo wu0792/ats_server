98eeed8430afb49b21170938538d3e8fe2861033
wu0792 <wu0792@163.com>
2018-08-03 09:05:41 +0800

    check file exist before compare file

--- src/analysis/compare.js
+++ src/analysis/compare.js
@@ -2,9 +2,27 @@ const fs = require('fs'),
     PNG = require('pngjs').PNG,
     pixelmatch = require('pixelmatch')
 
-function doCompare(id, fileName,index,count) {
-    let img1 = fs.createReadStream(`./expect/${id}/${fileName}`).pipe(new PNG()).on('parsed', compare),
-        img2 = fs.createReadStream(`./actual/${id}/${fileName}`).pipe(new PNG()).on('parsed', compare),
+function doCompare(id, fileName, index, count) {
+    let img1Path = `./expect/${id}/${fileName}`,
+        img2Path = `./actual/${id}/${fileName}`,
+        valid = true
+
+    if (!fs.existsSync(img1Path)) {
+        console.warn(`image not found at: ${img1Path}`)
+        valid = false
+    }
+
+    if (!fs.existsSync(img2Path)) {
+        console.warn(`image not found at: ${img2Path}`)
+        valid = false
+    }
+
+    if (!valid) {
+        return
+    }
+
+    let img1 = fs.createReadStream(img1Path).pipe(new PNG()).on('parsed', compare),
+        img2 = fs.createReadStream(img2Path).pipe(new PNG()).on('parsed', compare),
         filesRead = 0;
 
     function compare() {
@@ -13,9 +31,9 @@ function doCompare(id, fileName,index,count) {
 
         const differentPixelCount = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, { threshold: 0.1 });
         if (differentPixelCount === 0) {
-            console.log(`[√] ${index+1}/${count} ${fileName} equal.`)
+            console.log(`[√] ${index + 1}/${count} ${fileName} equal.`)
         } else {
-            console.log(`[×] ${index+1}/${count} ${fileName} has ${differentPixelCount} pixels difference.`)
+            console.log(`[×] ${index + 1}/${count} ${fileName} has ${differentPixelCount} pixels difference.`)
         }
 
         if (differentPixelCount > 0) {
@@ -33,7 +51,7 @@ function startCompare(id) {
         } else {
             const count = files.length
             files.forEach((file, index) => {
-                doCompare(id, file, index,count)
+                doCompare(id, file, index, count)
             })
         }
     })
