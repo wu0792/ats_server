6ad33849901da690009a02efb020e8fea85c19fd
wu0792 <wu0792@163.com>
2018-08-11 21:08:18 +0800

    add entry ui

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function createWindow() {
 
     mainWindow.loadURL(
         url.format({
-            pathname: path.join(__dirname, './html/main.html'),
+            pathname: path.join(__dirname, './html/index.html'),
             protocol: 'file:',
             slashes: true
         })
