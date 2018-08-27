5f4a0e75c3be05982d063bf419e549a9e1a0a1ea
wu0792 <wu0792@163.com>
2018-08-27 08:09:27 +0800

    add position judge for mouse down

--- src/common/querySelector.js
+++ src/common/querySelector.js
@@ -1,14 +1,19 @@
 const querySelector = async (page, validSelector) => {
     const elInfo = await page.evaluate((selector) => {
         let el = document.querySelector(selector),
-            nodeName, nodeWith, nodeHeight
+            nodeName, nodeWith, nodeHeight,
+            positionX, positionY
+
         if (el) {
             nodeName = el.nodeName
             nodeWith = el.offsetWidth
             nodeHeight = el.offsetHeight
+            boundingRect = el.getBoundingClientRect()
+            positionX = boundingRect.x
+            positionY = boundingRect.y
         }
 
-        return { scrollX, scrollY, nodeName, nodeWith, nodeHeight }
+        return { scrollX, scrollY, nodeName, nodeWith, nodeHeight, positionX, positionY }
     }, validSelector)
 
     return elInfo
